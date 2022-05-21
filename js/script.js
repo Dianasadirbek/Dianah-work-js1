b8c28d75a2c018dae92fcfec38fa28d05a305572
Diana Sadirbekkyzy <diana.sadirbekkyzy.kk@gmail.com>
2022-05-21 22:29:50 +0600

    1work added

---
+++ js/script.js
@@ -0,0 +1,40 @@
+// var userText = "你好！我是戴安娜。"
+// document.write(userText);
+// userText = " 我是学生和学习在69学校。谢谢您！";
+// document.write(userText);
+// let userName = "Diana";
+// let userEmail = "diana.sadirbekkyzy.kk@gmail.com";
+// document.write(userName + userEmail);
+// 4
+// let su = prompt("Введите количество сувениров?");
+// let bd = prompt("Введите количество безделушек?");
+
+//  const sv = 75;
+//  const be = 112;
+
+//  document.write(`Количество сувенира: ${su} грамм <br>`);
+//  document.write(`Количество безделушка: ${bd} грамм <br>`);
+//  document.write(`Вес одного сувенира: ${sv} грамм <br>`);
+//  document.write(`Вес одного безделушка: ${be} грамм <br>`);
+
+//  let sum = su * sv + bd * be;
+//  document.write (`Общий вес ${sum.toFixed(0)} <br>`);
+
+
+// 5
+ let a1 = prompt("Задайте значение(целых) для a");
+ let b2 = prompt("Задайте значение(целых) для b");
+
+ let a = Number(a1);
+ let b = Number(b2);
+
+ document.write("a=" + a + "<br>");
+ document.write("b=" + b + "<br>");
+ document.write("a+b=" + (a+b) + "<br>");
+ document.write("a-b=" + (a-b) + "<br>");
+ document.write("a*b=" + (a*b) + "<br>");
+ document.write("a/b=" + (a/b) + "<br>");
+ document.write("Частное от деления a/b=" + Math.trunc(a/b) + "<br>");
+ document.write("Частное от деления a*b=" + (a%b) + "<br>");
+ document.write("a^b=" + (a**b) + "<br>");
+
